0896334a30ffae1667b1b80683685ec2c1d50392
Grant DeLozier <grantdelozier@gmail.com>
2014-02-06 19:42:08 -0600

    BuildRef -- Now Accepts a -wordlist argument (file containing the set of words to include in model). Has two modes for running the calculation based on the Num Observations vs Num Features. GWRMain -- Made appropriate changes to reflect the new argument in BuildRef

--- BuildRef.py
+++ BuildRef.py
@@ -1,6 +1,6 @@
 import codecs
 import datetime
-#from itertools import chain
+from itertools import chain
 #import numpy
 import io
 import math
@@ -50,93 +50,28 @@ class Person:
 
         self.Feature_Prob = F_Prob
 
-
-
-
-def Build_ref_files(tf, rf_std_out, rf_obs_out):
-    print "Success Importing"
-    trainFile = tf
-    
-    #openTrain = codecs.open(trainFile, 'r', encoding='utf-8')
-    filename = trainFile[trainFile.rfind('/')+1:]
-    print filename
-    begin_time = datetime.datetime.now()
-    personList = []
-    x = 0
-    y = 0
-
-    #The length given here was observed from a smaller corpus of about 6000 people
-    F_All_Len = 114616
-
-    read_time_begin = datetime.datetime.now()
-    z = 0
-    
-    with io.open(trainFile, 'r', encoding='utf-8') as f:
-        for person in f:
-            x += 1
-            #print "####NEW Person####"
-            #print userID, latit, longit
-            try:
-                row = person.split('\t')
-                #print row[0]
-                userID = row[0]
-                latit = row[1].split(',')[0]
-                longit = row[1].split(',')[1]
-                F_Freq = dict(f.split(':') for f in row[2].split(" "))
-                #F_All = dict(chain(F_Freq.iteritems(), F_All.iteritems()))
-                #print len(F_Freq)
-                #F_Freq = {}
-                newPerson = Person(userID, latit, longit, F_Freq, filename, F_All_Len)
-                #newPerson.CalcProb('smoothed', 30, False, F_All_Len)
-                personList.append(newPerson)
-            except:
-                print "@@@@@error reading user@@@@@@"
-                print row
-                print z
-                break
-            z += 1
-            if z % 5000 == 0:
-                print z
-                print datetime.datetime.now()
-    print "Calculating Prob"
-
-    print "------Done reading in the data-------"
-    read_time_end = datetime.datetime.now()
-    print read_time_end - read_time_begin
-
-
-    Morans_List = []
-    print "Reading Morans List"
-    with io.open("morans_original_%10_whitelist.txt", 'r', encoding='utf-8') as w:
-        for line in w:
-            if line.strip() not in Morans_List:
-                Morans_List.append(line.strip())
-
-    print "Morans List Length:", len(Morans_List)
-
-
+def ModeCalc_2(F_All, personList, NumPeople, rf_obs_out, rf_std_out):
     print "Starting Calculation of mean probability"
     j = 0
     wordDict = {}
     start_mean = datetime.datetime.now()
-    for person in personList:
-        for word in Morans_List:
-            if j == 0:
-                if word in person.Feature_Prob:
-                    wordDict[word] = person.Feature_Prob[word]
-                else:
-                    wordDict[word] = person.outside_word_prob
-            else:
+    for word in F_All:
+        for person in personList:
+            if word in wordDict:
                 if word in person.Feature_Prob:
                     wordDict[word] += person.Feature_Prob[word]
                 else:
                     wordDict[word] += person.outside_word_prob
+            else:
+                if word in person.Feature_Prob:
+                    wordDict[word] = person.Feature_Prob[word]
+                else:
+                    wordDict[word] = person.outside_word_prob
         j += 1
         if j % 5000 == 0:
             print j
             print datetime.datetime.now()
 
-    NumPeople = len(personList)
     print "Total Mean Calc Time:"
     print datetime.datetime.now() - start_mean
 
@@ -182,5 +117,154 @@ def Build_ref_files(tf, rf_std_out, rf_obs_out):
             print "Broken word: ", i
         
     std_dev_outfile.close()
+
+    print "Total Std Dev Time: ", start_mean - datetime.datetime.now() 
+
+def ModeCalc_1(F_All, personList, NumPeople, rf_obs_out, rf_std_out):
+    print "Starting Calculation of mean probability"
+    j = 0
+    wordDict = {}
+    start_mean = datetime.datetime.now()
+    for person in personList:
+        for word in F_All:
+            if j == 0:
+                if word in person.Feature_Prob:
+                    wordDict[word] = person.Feature_Prob[word]
+                else:
+                    wordDict[word] = person.outside_word_prob
+            else:
+                if word in person.Feature_Prob:
+                    wordDict[word] += person.Feature_Prob[word]
+                else:
+                    wordDict[word] += person.outside_word_prob
+        j += 1
+        if j % 5000 == 0:
+            print j
+            print datetime.datetime.now()
+
+    print "Total Mean Calc Time:"
+    print datetime.datetime.now() - start_mean
+
+    obs_ref_file = codecs.open(rf_obs_out, 'w', encoding='utf-8')
+
+    
+    j = 0
+    wordStds = {}
+    print "Starting Std Dev Calculations"
+    start_mean = datetime.datetime.now()
+    z = 0
+    for person in personList:
+        obs_ref_file.write((person.userID).encode('utf-8').decode('utf-8') + '\t' + str(z) + '\r\n')
+        z += 1
+        for word in wordDict:
+            if j == 0:
+                wordDict[word] = float(wordDict[word]) / float(NumPeople)
+                if word in person.Feature_Prob:
+                    wordStds[word] = math.pow((person.Feature_Prob[word] - wordDict[word]), 2)
+                else:
+                    wordStds[word] = math.pow((float(person.outside_word_prob) - wordDict[word]), 2)
+            else:
+                if word in person.Feature_Prob:
+                    wordStds[word] += math.pow((float(person.Feature_Prob[word]) - wordDict[word]), 2)
+                else:
+                    wordStds[word] += math.pow((float(person.outside_word_prob) - wordDict[word]), 2)
+        j += 1
+        if j % 5000 == 0:
+            print j
+            print datetime.datetime.now()
+
+    obs_ref_file.close()
+
+    std_dev_outfile = codecs.open(rf_std_out, 'w', encoding='utf-8')
+
+    print "Number words in model:", len(wordStds)
+
+    for i in wordStds:
+        try:
+            std_dev_outfile.write((i.encode('utf-8').decode('utf-8') + '\t' + str(wordDict[i]) + '\t' + str(math.sqrt(wordStds[i]/float(NumPeople))) + '\r\n'))
+        except:
+            print i, type(i)
+            print "Broken word: ", i
+        
+    std_dev_outfile.close()
+
+    print "Total Std Dev Time: ", start_mean - datetime.datetime.now() 
+
+
+def GetWords(wordlist):
+    Words_List = []
+    print "Reading Word List"
+    with io.open(wordlist, 'r', encoding='utf-8') as w:
+        for line in w:
+            if line.strip() not in Words_List:
+                Words_List.append(line.strip())
+
+    print "Word List Length:", len(Words_List)
+    return Words_List
+
+def Build_ref_files(tf, rf_std_out, rf_obs_out, wordlist):
+    print "Success Importing"
+    trainFile = tf
+    
+    #openTrain = codecs.open(trainFile, 'r', encoding='utf-8')
+    filename = trainFile[trainFile.rfind('/')+1:]
+    print filename
+    begin_time = datetime.datetime.now()
+    personList = []
+    x = 0
+    y = 0
+
+    #The length given here was observed from a smaller corpus of about 6000 people
+    F_All_Len = 114616
+
+    read_time_begin = datetime.datetime.now()
+    z = 0
+    
+    with io.open(trainFile, 'r', encoding='utf-8') as f:
+        for person in f:
+            x += 1
+            #print "####NEW Person####"
+            #print userID, latit, longit
+            try:
+                row = person.split('\t')
+                #print row[0]
+                userID = row[0]
+                latit = row[1].split(',')[0]
+                longit = row[1].split(',')[1]
+                F_Freq = dict(f.split(':') for f in row[2].split(" "))
+                if wordlist == 'any':
+                    F_All = dict(chain(F_Freq.iteritems(), F_All.iteritems()))
+                #print len(F_Freq)
+                #F_Freq = {}
+                newPerson = Person(userID, latit, longit, F_Freq, filename, F_All_Len)
+                #newPerson.CalcProb('smoothed', 30, False, F_All_Len)
+                personList.append(newPerson)
+            except:
+                print "@@@@@error reading user@@@@@@"
+                print row
+                print z
+                break
+            z += 1
+            if z % 5000 == 0:
+                print z
+                print datetime.datetime.now()
+    print "Calculating Prob"
+
+    print "------Done reading in the data-------"
+    read_time_end = datetime.datetime.now()
+    print read_time_end - read_time_begin
+
+    if wordlist != 'any':
+        F_All = GetWords(wordlist)
+
+    NumPeople = len(personList)
+    
+    if NumPeople > len(F_All):
+        print "Starting on Mode 1 (More Observations Than Variables)"
+        ModeCalc_1(F_All, personList, NumPeople, rf_obs_out, rf_std_out)
+    else:
+        print "Starting on Mode 2 (More Variables Than Observations)"
+        ModeCalc_2(F_All, personList, NumPeople, rf_obs_out, rf_std_out)
+
     print "Done Writing Std Dev File"
 
